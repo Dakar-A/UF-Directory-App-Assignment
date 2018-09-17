3d8f7d6d53156dbbce97948f51682a1d9b245f46
Dakar-A <duncan@Duncans-MacBook-Pro.local>
2018-09-17 18:48:58 -0400

    Completed all requirements
    JSONtoMongo, ListingSchema, and queries have all been updated!

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -10,12 +10,22 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
-
+mongoose.connect('mongodb://duncanjadkins:LegoManiac12@ds161322.mlab.com:61322/da-assignment3');
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
-
+fs.readFile('listings.json', function(err, data){
+  var listings = JSON.parse(data);
+  listings.entries.forEach(function(listing){
+    var listingInstance = new Listing(listing);
+    listingInstance.save(function(err)){
+      if(err)
+        throw(err);
+    });
+  });
+});
+process.exit();
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
